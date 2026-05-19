bc5803ebd2ca47f9041a050c8776d21c6a7a2152
Curtis <curtis_102@hotmail.com>
2026-05-19 12:44:53 -0700

    preloading added

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
-import { provideRouter, withViewTransitions } from '@angular/router';
+import { provideRouter, withPreloading, PreloadAllModules, withViewTransitions } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideFirebase } from './core/firebase';
@@ -7,7 +7,7 @@ import { provideFirebase } from './core/firebase';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes, withViewTransitions()),
+    provideRouter(routes, withPreloading(PreloadAllModules), withViewTransitions()),
     provideFirebase(),
   ],
 };
